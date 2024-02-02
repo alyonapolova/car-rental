7a378094d5cf80dbbcfe744a0ebb7f3604fc82eb
Alyona <polova_alyona0907@ukr.net>
2024-02-02 11:21:31 +0100

    favList

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,25 +1,27 @@
-// import { useDispatch } from 'react-redux';
-// import { addToFavorites } from 'redux/slice';
+import { useDispatch } from 'react-redux';
+import { addToFavorites } from '../redux/slice';
 
-// export const CarCard = ({ car }) => {
-//   const dispatch = useDispatch();
-//   const handleAddToFavorites = () => {
-//     dispatch(addToFavorites(car));
-//   };
-//   return (
-//     <div>
-//       <img src={car.img} width="150" alt={car.model} />
-//       <button type="button" onClick={handleAddToFavorites}>
-//         add to fav
-//       </button>
-//       <h2>{car.make}</h2>
-//       <h2>{car.rentalPrice}</h2>
-//       <p>{car.address}</p>
-//       <p>{car.rentalCompany}</p>
-//       <p>{car.type}</p>
-//       <p>{car.model}</p>
-//       <p>{car.mileage}</p>
-//       <p>{car.accessories[0]}</p>
-//     </div>
-//   );
-// };
+export const CarCard = ({ car }) => {
+  const dispatch = useDispatch();
+  const handleAddToFavorites = () => {
+    dispatch(addToFavorites(car));
+  };
+
+  console.log(car);
+  return (
+    <div>
+      <img src={car.img} width="150" alt={car.model} />
+      <button type="button" onClick={handleAddToFavorites}>
+        add to fav
+      </button>
+      <h2>{car.make}</h2>
+      <h2>{car.rentalPrice}</h2>
+      <p>{car.address}</p>
+      <p>{car.rentalCompany}</p>
+      <p>{car.type}</p>
+      <p>{car.model}</p>
+      <p>{car.mileage}</p>
+      <p>{car.accessories[0]}</p>
+    </div>
+  );
+};
--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,9 +1,18 @@
+import { CarCard } from './CarCard';
+
 export const Catalog = ({ cars }) => {
   console.log(cars);
   return (
     <div>
       <h1>Catalog</h1>
-      <ul>{cars && cars.map(car => <li key={car.id}>{car.model}</li>)}</ul>
+      <ul>
+        {cars &&
+          cars.map(car => (
+            <li key={car.id}>
+              <CarCard car={car} />
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
