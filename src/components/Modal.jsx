d99b730cdcd3fa9ed861414ded47e60dced18ea2
Alyona <polova_alyona0907@ukr.net>
2024-02-04 00:38:13 +0100

    modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,35 +1,81 @@
 import { useSelector } from 'react-redux';
 import { carSelector } from '../redux/selectors';
-import { Backdrop, ModalStyled } from './styles/Modal.styled';
+import {
+  Accessories,
+  AccessoriesList,
+  AccessoryItem,
+  Backdrop,
+  BtnClose,
+  Desc,
+  InfoDiv,
+  InfoDiv2,
+  ModalImg,
+  ModalStyled,
+  RentBtn,
+  Styled1,
+  StyledSpan2,
+  StyledSpan3,
+  TextStyle,
+  TitleDiv,
+} from './styles/Modal.styled';
 
 export const Modal = ({ isModal, onClose }) => {
   const cardInfo = useSelector(carSelector);
 
+  // const handleKeyDown = event => {
+  //   if (event.key === 'Escape') {
+  //     onClose();
+  //   }
+  // };
+
   if (!isModal) return null;
-  console.log(isModal);
+
   return (
-    <Backdrop isModal={isModal}>
-      <ModalStyled isModal={isModal}>
-        <button type="button" onClick={() => onClose()}>
+    <Backdrop isModal={isModal} onClick={onClose}>
+      <ModalStyled isModal={isModal} onClick={e => e.stopPropagation()}>
+        <BtnClose type="button" onClick={onClose}>
           &#9421;
-        </button>
-        <img src={cardInfo.img} alt={cardInfo.model} />
-        <p>
-          {cardInfo.make} {cardInfo.model},{cardInfo.year}
-        </p>
-        <div>
-          <p>{cardInfo.address}</p>
-          <p>{cardInfo.rentalCompany}</p>
-          <p>{cardInfo.type}</p>
-          <p>{cardInfo.mileage}</p>
-          <p>{cardInfo.accessories[0]}</p>
-        </div>
-        <p>{cardInfo.description}</p>
-        <h2>Accessories and functionalities:</h2>
-        <p>{cardInfo.accessories}</p>
-        <h2>Rental Conditions: </h2>
-        <p>{cardInfo.rentalConditions}</p>
-        <p>{cardInfo.mileage}</p>
+        </BtnClose>
+        <ModalImg src={cardInfo.img} alt={cardInfo.model} />
+        <TitleDiv>
+          <Styled1>
+            {cardInfo.make} <StyledSpan2>{cardInfo.model}</StyledSpan2>,
+            {cardInfo.year}
+          </Styled1>{' '}
+        </TitleDiv>
+        <InfoDiv>
+          <TextStyle>{cardInfo.address}</TextStyle>
+          <TextStyle>{cardInfo.year}</TextStyle>
+          <TextStyle>{cardInfo.rentalCompany}</TextStyle>
+          <TextStyle>{cardInfo.type}</TextStyle>
+          <TextStyle>{cardInfo.fuelConsumption}</TextStyle>
+          <TextStyle>{cardInfo.engineSize}</TextStyle>
+        </InfoDiv>
+        <Desc>{cardInfo.description}</Desc>
+        <Accessories>Accessories and functionalities:</Accessories>
+        <AccessoriesList>
+          {cardInfo.accessories.map((accessory, index) => (
+            <AccessoryItem key={index}>{accessory}</AccessoryItem>
+          ))}
+        </AccessoriesList>
+        <Accessories>Rental Conditions: </Accessories>
+        <InfoDiv2>
+          {cardInfo.rentalConditions.split('\n').map((condition, index) => (
+            <TextStyle key={index}>
+              {condition}
+              <br />
+            </TextStyle>
+          ))}
+          <TextStyle>
+            <StyledSpan3>mileage: </StyledSpan3>
+            {cardInfo.mileage.toLocaleString('en-US')}
+          </TextStyle>
+          <TextStyle>
+            <StyledSpan3>price:</StyledSpan3> {cardInfo.rentalPrice}
+          </TextStyle>
+        </InfoDiv2>
+
+        <RentBtn href="tel:+380730000000">Rental car</RentBtn>
       </ModalStyled>
     </Backdrop>
   );
--- src/components/styles/Modal.styled.js
+++ src/components/styles/Modal.styled.js
@@ -5,25 +5,29 @@ export const Backdrop = styled.div`
   width: 100%;
   height: 100%;
   top: 0;
-  background-color: rgba(46, 47, 66, 0.4);
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #12141780;
   transition-property: opacity, visibility;
   transition-duration: 250ms;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   opacity: ${props => (props.isModal ? 1 : 0)};
   visibility: ${props => (props.isModal ? 'visible' : 'hidden')};
   pointer-events: ${props => (props.isModal ? 'auto' : 'none')};
+  z-index: 10001;
 `;
 
 export const ModalStyled = styled.div`
   position: absolute;
-  padding: 72px 16px 24px;
-  width: 392px;
-  min-height: 584px;
+  padding: 40px;
+  width: 541px;
 
+  border-radius: 24px;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: red;
+  background-color: white;
   box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12),
     0px 2px 1px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
@@ -34,5 +38,117 @@ export const ModalStyled = styled.div`
   opacity: ${props => (props.isModal ? 1 : 0)};
   visibility: ${props => (props.isModal ? 'visible' : 'hidden')};
   pointer-events: ${props => (props.isModal ? 'auto' : 'none')};
-  z-index: 999;
+`;
+
+export const ModalImg = styled.img`
+  border-radius: 14px;
+  margin-bottom: 14px;
+`;
+
+export const BtnClose = styled.button`
+  position: absolute;
+  border: none;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    font-size: 25px;
+  }
+  &:focus {
+    font-size: 25px;
+  }
+`;
+
+export const TitleDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Styled1 = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  margin-right: 2px;
+`;
+
+export const StyledSpan2 = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+  margin-right: 2px;
+  color: #3470ff;
+`;
+
+export const StyledSpan3 = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+  margin-right: 2px;
+  color: #3470ff;
+`;
+export const InfoDiv = styled.div`
+  max-width: 277px;
+  margin-top: 8px;
+  margin-bottom: 14px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const InfoDiv2 = styled.div`
+  max-width: 277px;
+  margin-top: 8px;
+  margin-bottom: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+export const TextStyle = styled.p`
+  border-right: 1px solid #1214171a;
+  padding-right: 6px;
+  font-size: 12px;
+  color: #12141780;
+`;
+
+export const Desc = styled.p`
+  font-size: 14px;
+  margin-bottom: 24px;
+`;
+
+export const Accessories = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  margin-bottom: 8px;
+`;
+
+export const AccessoriesList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 277px;
+  margin-bottom: 24px;
+`;
+
+export const AccessoryItem = styled.p`
+  border-right: 1px solid #1214171a;
+  padding-right: 6px;
+  font-size: 12px;
+  color: #12141780;
+`;
+
+export const RentBtn = styled.a`
+  display: inline-block;
+
+  background-color: #3470ff;
+  color: white;
+  font-size: 14px;
+  border-radius: 12px;
+  padding: 12px 50px;
+  border: none;
+  &:hover {
+    background-color: #0b44cd;
+  }
+  &:focus {
+    background-color: #0b44cd;
+  }
 `;
